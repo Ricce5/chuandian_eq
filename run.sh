c35a97c6adf02026a03802baaa7ef316763ec20f
yizhizhang <yizhi_zhang@mail.nwpu.edu.cn>
2025-05-21 20:54:07 +0800

    5.21.20.54

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-python main.py --model FGCN_f --mode test --checkpoint '/home/yzzhang/pjt/python_L/Eq_sp_f/checkpoints/fgcn_f_20250513-102216'
-
-python main.py --model FGCN --mode test --checkpoint '/home/yzzhang/pjt/python_L/Eq_sp_f/checkpoints/fgcn_20250512-235030'
-
-python main.py --model TGCN --mode test --checkpoint '/home/yzzhang/pjt/python_L/Eq_sp_f/checkpoints/tgcn_20250512-224841'
+python main.py --model Classifier --mode train
 
 
 
