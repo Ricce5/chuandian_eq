69dc6ebbf6dec34aa8fcd517451209427cab7f2a
yzzhang <zyz000678@gmail.com>
2025-06-10 11:28:43 +0800

    6.10.11

--- src/models/SubLayers.py
+++ src/models/SubLayers.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+import math
+
+
 
 class CNNBlock(nn.Module):
     def __init__(self, c_in, c_out):
@@ -16,3 +19,23 @@ class CNNBlock(nn.Module):
         x =  self.cnn(x.permute(0, 2, 1)).transpose(1,2) 
         return x
     
+class ScaledSoftplus(nn.Module):
+    '''
+    Use different beta for mark-specific intensities
+    '''
+    def __init__(self, num_marks, threshold=20.):
+        super(ScaledSoftplus, self).__init__()
+        self.threshold = threshold
+        self.log_beta = nn.Parameter(torch.zeros(num_marks), requires_grad=True)  # [num_marks]
+
+    def forward(self, x):
+        '''
+        :param x: [..., num_marks]
+        '''
+        beta = self.log_beta.exp()
+        beta_x = beta * x
+        return torch.where(
+            beta_x <= self.threshold,
+            torch.log1p(beta_x.clamp(max=math.log(1e5)).exp()) / beta,
+            x,  # if above threshold, then the transform is effectively linear
+        )
