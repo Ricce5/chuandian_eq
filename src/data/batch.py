33a78e4676723f051690bc187d61acaa55945040
yzzhang <zyz000678@gmail.com>
2025-06-13 11:14:02 +0800

    6.13.11

--- src/data/batch.py
+++ src/data/batch.py
@@ -93,12 +93,15 @@ class Batch(DotDict):
             )
 
         non_pad_mask = (inter_times != 0).float()
+        non_pad_mask[:, 0] = 1.  # 确保第一个事件不是 padding
+
 
         if "type_event" in other_attr:
             type_event = other_attr["type_event"]  # shape: [batch_size, padded_seq_len]
 
             type_seq = torch.full_like(type_event, fill_value=PAD_TOKEN_ID)
             type_seq[non_pad_mask.bool()] = type_event[non_pad_mask.bool()].long()
+            # del other_attr["type_event"]
         else:
             type_seq = build_type_seq(non_pad_mask, pad_token_id=PAD_TOKEN_ID)
 
