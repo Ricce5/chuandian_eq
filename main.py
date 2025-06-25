f792343bdf015acc0e33e50634f815d6ec13b4f2
yzzhang <zyz000678@gmail.com>
2025-06-25 17:56:12 +0800

    6.25.17

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
             writer=writer,
         )
     elif args_cli.mode == "test":
-        checkpoint_path = f"{args.save_dir}/best_model_{args_cli.trial_index}.pth"  #  last/best
+        checkpoint_path = f"{args.save_dir}/last_model_{args_cli.trial_index}.pth"  #  last/best
         checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=False)
         args = config_setup.load_args_from_checkpoint(args, checkpoint)
         args.use_sampler = False
