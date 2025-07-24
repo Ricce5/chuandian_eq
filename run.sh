bf782ab5c1c73aaf101cac7e0555c2ed494e36bc
yzzhang <zyz000678@gmail.com>
2025-07-24 19:08:00 +0800

    无改动

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ python main.py --model clf_attnpl_t --mode train
 python main.py --model classifier_se --mode train
 python main.py --model classifier_stm --mode train
 python main.py --model classifier_tm_s --mode train
+python main.py --model thp --mode train
