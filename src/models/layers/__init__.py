65734a5a48f20df86671a79a20d0967e352058de
yzzhang <zyz000678@gmail.com>
2025-07-25 18:54:52 +0800

    'clf_tm_attnpl_t在scedc和chuandian实验'修正权test权重加载时load_specific_parts的错误

--- src/models/layers/__init__.py
+++ src/models/layers/__init__.py
@@ -2,7 +2,6 @@ from .cnn import CNN
 from .mlp import MLP
 from .gcn import GCN
 from .attention_pooling import AttentionPooling
-from .cnn_block import CNNBlock
 from .activations import ScaledSoftplus
 
 
